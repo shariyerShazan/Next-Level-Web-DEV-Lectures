5d31bbf9a129a6b4b365cd3b501d80c5e1f9fc63
shariyerShazan <shariyershazan1@gmail.com>
2025-11-10 07:14:35 +0600

    lecture - 3

---
+++ src/2_session/1_lecture/3_index.ts
@@ -0,0 +1,16 @@
+// lecture 3 // ! primitive data type
+//? JS -> TS
+
+//*  JS types: string , number , boolean , undifiend , null 
+//! TS types: never , unknown , void , any
+
+
+// let userId = 1234
+// userId = "1234" //? Type 'string' is not assignable to type 'number'
+
+// let x; //! let x: any
+
+// let y : undefined ;
+// y = 5 ;  //? Type '5' is not assignable to type 'undefined'.
+// y = "shazan" //? Type '"shazan"' is not assignable to type 'undefined'.
+
