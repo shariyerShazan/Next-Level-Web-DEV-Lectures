647a5880b823c15ae24106005c299ff604106d56
shariyerShazan <shariyershazan1@gmail.com>
2025-11-10 07:32:17 +0600

    lecture 4

--- src/2_session/1_lecture/4_index.ts
+++ src/2_session/1_lecture/4_index.ts
@@ -6,7 +6,7 @@
 
 // let fruit : string[] = ["apple" , "bannana" , "pineapple" , "mango"] 
 //? type defined only for 
-//!sting array
+//! sting array
 
 
 // let fruit = ["apple" , "bannana" , "pineapple" , "mango"]
@@ -21,8 +21,33 @@
 // data.push(true) //?Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
 
 
-let data : (string | number)[] = ["apple" , 5 , "banana" , 10 , "mango" , 21]
+// let data : (string | number)[] = ["apple" , 5 , "banana" , 10 , "mango" , 21]
 //? type defined only for 
 //! sting | number array
 
 
+// let cordinate : [number , number] = [20 , 30 , 20]
+//? 'cordinate' is declared but its value is never read.ts(6133)
+//?  Type '[number, number, number]' is not assignable to type '[number, number]'.
+//? Source has 3 element(s) but target allows only 2.
+
+
+// let cordinate : [number , number] = [20 , 30 ] //TODO=>  no error
+
+
+// let nameAndRoll : [string , number] = ["shazan" , 29] //TODO=>  no error
+// let nameAndRoll : [string , number] = ["shazan" , "29"] //? Type 'string' is not assignable to type 'number'.
+
+
+// let nameAndRoll : [string , number][] = [["shazan" , 29 ], ["priya" , 34]] //TODO=>  no error
+
+
+// let nameAndRoll : [string , number] = ["shazan" , 29 , "priya" , 34]
+//?  Type '[string, number, string, number]' is not assignable to type '[string, number]'.
+//?  Source has 4 element(s) but target allows only 2.
+
+
+
+
+// let threeD : [number , number , number] = [5 , 7, 3] //TODO=>  no error
+// let threeD : [number , number , number] = ["shazan" , 7, 3] //! Type 'string' is not assignable to type 'number'
