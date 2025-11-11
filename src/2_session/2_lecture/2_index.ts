69d76c0d41dfa71d7bdb27f1480bc206d6052947
shariyerShazan <shariyershazan1@gmail.com>
2025-11-11 11:04:40 +0600

    interface

---
+++ src/2_session/2_lecture/2_index.ts
@@ -0,0 +1,40 @@
+//! type alias..
+// type User = {
+//   fullName: string;
+//   email: string;
+//   age: number;
+// };
+
+//! interface
+// interface IUser  {
+//     fullName: string;
+//     email: string;
+//     age: number;
+// }
+
+
+// type Role = {
+//   role: "admin" | "user";
+// };
+// type UserWithRole = User & Role;
+
+
+// const userOne: UserWithRole = {
+//   fullName: "shariyer shazan",
+//   email: "shazan@gmail.com",
+//   age: 22,
+//   role: "admin",
+// };
+
+// const userTwo: IUser = {
+//     fullName: "shariyer" ,
+//     email: "shazan@gmail.com" ,
+//     age: 22
+// }
+
+
+
+
+
+//! interface just take object
+// interface User = string //! its getting error! .. interface have no "=" .. 
