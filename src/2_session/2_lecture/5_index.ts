7cf6f1f5fe9c636dc77f6c87ccacd28117754919
shariyerShazan <shariyershazan1@gmail.com>
2025-11-12 10:51:03 +0600

    generic types

---
+++ src/2_session/2_lecture/5_index.ts
@@ -0,0 +1,29 @@
+//! generic function
+
+
+
+const createArryWithString = (value : string)=> { //? every types need a apecific function
+    return value
+}
+createArryWithString("ok")
+const createArryWithNumber = (value : number)=> { //? every types need a apecific function
+    return value
+}
+createArryWithNumber(20)
+
+
+
+
+
+const createArrWithGeneric = <T>(value: T) => {//! no need specific function . one function and multiple types
+    return [value];
+  }
+const res = createArrWithGeneric("string") // const res: string[]
+
+
+
+
+const createArr = <X , Y>(value1 : X , value2: Y) => { //! no need specific function . one function and multiple types
+      return [value1 , value2]
+}
+let res = createArr("shazan" , 22) // let res: (string | number)[]
